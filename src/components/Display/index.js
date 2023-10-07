69b3558b39a1756ec6e2bfcf7065089cdb784b43
Anu2640 <anuprathikantam@gmail.com>
2023-10-07 13:19:57 +0530

    reactjs 1

---
+++ src/components/Display/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+
+const Display = () => (
+    <div className='bg-container'>
+        <h1>Super Over League</h1>
+        <div className='image-container'>
+            <img src='https://assets.ccbp.in/frontend/react-js/rcb-img.png'className='image'alt='photo1'/>
+            <img src='https://assets.ccbp.in/frontend/react-js/csk-img.png'className='image'alt='photo2'/>
+        </div>
+    </div>
+)
+
+export default Display;
